73cf9ed850ed1420092e1e82e701e3c9bb67b37e
Marwan-Alzain-AI-journey <customer.mrwan11@gmail.com>
2026-06-29 02:51:25 +0300

    busy day :)

---
+++ day3.py
@@ -0,0 +1,29 @@
+e1 = [1, 4, 5, 6]
+print(e1)
+e1.append(7)
+print(e1)
+e2 = (33, 33, 55, 77, 88)
+print(e2)
+print(e2.count(33))
+
+
+e3 = [(5, 5), (4, 3), (6, 7)]
+e3.append((9, 9))
+print(e3)
+
+
+def f_3(name, age):
+    print("hi there your name is " + name + " you are " + str(age) + " years old")
+
+
+f_3(input("enter your name: " ), input("how old are you: " ))
+
+
+
+print(pow(2, 3))
+print(3 * 3)
+
+def cup(num):
+     e1 = pow(num, 4)
+     print(e1)
+cup(3)
